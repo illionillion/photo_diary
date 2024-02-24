aae00e5fcdaebf571e81d4bfde00f3e1a08ceff8
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-24 09:26:28 +0900

    fix: カルーセルの背景色変更

--- www/html/diary-list.php
+++ www/html/diary-list.php
@@ -78,11 +78,11 @@ try {
                                 <?php endforeach;?>
                             </div>
                             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample<?= $diary["diary_id"] ?>" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="carousel-control-prev-icon bg-primary" aria-hidden="true"></span>
                                 <span class="visually-hidden">Previous</span>
                             </button>
                             <button class="carousel-control-next" type="button" data-bs-target="#carouselExample<?= $diary["diary_id"] ?>" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="carousel-control-next-icon bg-primary" aria-hidden="true"></span>
                                 <span class="visually-hidden">Next</span>
                             </button>
                         </div>
